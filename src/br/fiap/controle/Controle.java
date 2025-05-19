c25a96ce7b18a391482b545422870e1cc27a6451
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-19 19:39:02 -0300

    commit

---
+++ src/br/fiap/controle/Controle.java
@@ -0,0 +1,60 @@
+package br.fiap.controle;
+import br.fiap.assento.Assento;
+import br.fiap.cliente.Cliente;
+import br.fiap.cliente.PessoaFisica;
+import br.fiap.cliente.PessoaJuridica;
+import br.fiap.reserva.Reserva;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.Double.parseDouble;
+import static javax.swing.JOptionPane.*;
+
+public class Controle {
+
+    public void menu() {
+        int opcao;
+
+        while(true) {
+            try {
+                opcao = parseInt(showInputDialog(gerarMenu()));
+                switch(opcao) {
+                    case 1:
+                        reservar();
+                        break;
+                    case 2:
+                        pesquisar();
+                        break;
+                    case 3:
+                        cancelar();
+                        break;
+                    case 4:
+                        return;
+                    default:
+                        showMessageDialog(null, "Opção inválida");
+                }
+            }
+            catch(NumberFormatException e) {
+                showMessageDialog(null, "você deve digitar um número");
+            }
+        }
+    }
+
+    private void cancelar() { }
+
+    private void pesquisar() { }
+
+    private void reservar() { }
+
+    private String gerarMenu() {
+        String aux = "SISTEMA DE RESERVA DE PASSAGEM AÉREA\n";
+        aux += "1. Reservar\n";
+        aux += "2. Pesquisar reserva\n";
+        aux += "3. Cancelar reserva\n";
+        aux += "4. Finalizar";
+        return aux;
+    }
+}
---
+++ src/br/fiap/reserva/Reserva.java
@@ -0,0 +1,75 @@
+package br.fiap.reserva;
+
+import br.fiap.assento.Assento;
+import br.fiap.cliente.Cliente;
+import br.fiap.cliente.PessoaFisica;
+import br.fiap.desconto.Desconto;
+
+import java.text.DecimalFormat;
+
+public class Reserva {
+    private Cliente cliente;
+    private double valorOriginal;
+    private double valorFinal;
+    private Assento assento;
+
+    public Reserva(Cliente cliente, double valorOriginal, Assento assento) {
+        this.cliente = cliente;
+        this.valorOriginal = valorOriginal;
+        this.assento = assento;
+        aplicarDesconto(valorOriginal);
+    }
+
+    private void aplicarDesconto(double valorOriginal) {
+        if(cliente instanceof Desconto) {
+            Desconto desconto = (Desconto) cliente;
+            valorFinal = desconto.aplicarDesconto(valorOriginal);
+        }
+        else {
+            valorFinal = valorOriginal;
+        }
+    }
+
+    @Override
+    public String toString() {
+        DecimalFormat df = new DecimalFormat("#,##0.00");
+        String aux = "";
+        aux += "Cliente: " + cliente.getNome() + "\n";
+        aux += "Assento: " + assento.getNumero() + "\n";
+        aux += "Valor Original: R$ " + df.format(valorOriginal) + "\n";
+        aux += "Valor Final: R$ " + df.format(valorFinal) + "\n";
+        return aux;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public double getValorOriginal() {
+        return valorOriginal;
+    }
+
+    public void setValorOriginal(double valorOriginal) {
+        this.valorOriginal = valorOriginal;
+    }
+
+    public double getValorFinal() {
+        return valorFinal;
+    }
+
+    public void setValorFinal(double valorFinal) {
+        this.valorFinal = valorFinal;
+    }
+
+    public Assento getAssento() {
+        return assento;
+    }
+
+    public void setAssento(Assento assento) {
+        this.assento = assento;
+    }
+}
